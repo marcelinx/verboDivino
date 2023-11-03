5146cdfe7b7a235e6c0d67ae995804d26245d5b7
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-11-03 08:27:00 -0300

    Criacao de Componente e Rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: "",
-    component: NavComponent
-  }
+    path: '',
+    component: NavComponent,
+    children: [{
+      path: '',
+      component: HomeComponent
+    }],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
