0bca97544ff18588e5960bcdf5b434ef6680da1c
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-11-04 14:06:31 -0300

    otimizando codigo

---
+++ src/app/components/constants/messages.constants.ts
@@ -0,0 +1,28 @@
+export const cabecalho = {
+  titulo01: 'Página Inicial',
+  titulo02: 'Contato',
+  titulo03: 'Eventos',
+  titulo04: 'Serviços',
+  titulo05: 'Login'
+}
+
+export const telaPrincipal = {
+  bemVindo: 'Bem-vindo à Verbo Divino',
+  sigaRede: 'Nos acompanhe em nossas redes!',
+  sobreNos: 'Sobre Nós',
+  contandoSobre: `Somos uma comunidade dedicada a espalhar a palavra de Deus e a promover o
+  amor e a compaixão entre as pessoas. Nossa igreja foi fundada em 19XX e
+  desde então temos crescido e prosperado, atendendo às necessidades
+  espirituais de nossa comunidade. Acreditamos na importância da oração, do
+  estudo da Bíblia e da ajuda mútua. Nossos valores incluem a igualdade, a
+  compaixão e o respeito pelas diferenças. Venha se juntar a nós em nossos
+  cultos, grupos de estudo e eventos especiais. Estamos ansiosos para
+  conhecê-lo e compartilhar nossa fé com você.`,
+  servicos: 'Nossos Serviços',
+  descreveServicos: 'Oferecemos uma variedade de serviços para atender às necessidades espirituais de nossa comunidade, incluindo cultos, grupos de estudo e programas sociais.',
+  eventoTitulo: 'Próximos Eventos',
+  contatoTitulo: 'Contato',
+  contatoDescricao: 'Estamos aqui para ajudar. Entre em contato conosco se tiver alguma pergunta ou necessidade.',
+  contatoEmail: 'Email: contato@igrejasagrada.com',
+  contatoTelefone: 'Telefone: (XX) XXXX-XXXX',
+};
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { telaPrincipal } from '../constants/messages.constants';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  messages = telaPrincipal;
 }
