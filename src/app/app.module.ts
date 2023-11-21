8f6580e10948f5599601542eea886f5ed5183560
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-11-21 10:53:50 -0300

    melhorando a utilidade dos arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
+
 import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import { MainScreenComponent } from './components/mainScreen/mainscreen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavComponent,
-    HomeComponent
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mainScreen/mainscreen.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mainscreen',
+  templateUrl: './mainscreen.component.html',
+  styleUrls: ['./mainscreen.component.scss'],
+})
+export class MainScreenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
